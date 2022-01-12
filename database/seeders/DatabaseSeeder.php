8d3e3992af41fd8bb720cf7252d1d89228904bfd
abrahamkivosh <abrahamkivosh@gmail.com>
2022-01-12 12:55:43 +0300

    mod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Event;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-            // User::factory(10)->create();
+            // User::factory(3)->create();
             Event::factory(10)->create();
+            User::create([
+
+                'first_name' => "Admin",
+                    'last_name' => "Peter",
+                    'middle_name' => "kamau",
+                    'dob' => now(),
+                    'phone' => "0725632145",
+                    'email' => "admin@test.com",
+                    'role'=> 1,
+                    'password' => Hash::make("password"),
+
+            ]) ;
+
     }
 }
--- resources/views/dashboard1.blade.php
+++ resources/views/dashboard1.blade.php
@@ -11,16 +11,15 @@
         <!-- ============================================================== -->
         <div class="row page-titles">
             <div class="col-md-5 align-self-center">
-                <h4 class="text-themecolor">Dashboard 1</h4>
+                <h4 class="text-themecolor">Dashboard </h4>
             </div>
             <div class="col-md-7 align-self-center text-right">
                 <div class="d-flex justify-content-end align-items-center">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard 1</li>
+                        <li class="breadcrumb-item active">Dashboard</li>
                     </ol>
-                    <button type="button" class="btn btn-info d-none d-lg-block m-l-15"><i
-                            class="fa fa-plus-circle"></i> Create New</button>
+                   
                 </div>
             </div>
         </div>
@@ -40,8 +39,8 @@
                             <div class="round align-self-center round-success"><i class="ti-wallet"></i>
                             </div>
                             <div class="m-l-10 align-self-center">
-                                <h3 class="m-b-0">3564</h3>
-                                <h5 class="text-muted m-b-0">New Customers</h5>
+                                <h3 class="m-b-0">3</h3>
+                                <h5 class="text-muted m-b-0">New Members</h5>
                             </div>
                         </div>
                     </div>
@@ -56,8 +55,8 @@
                             <div class="round align-self-center round-info"><i class="ti-user"></i>
                             </div>
                             <div class="m-l-10 align-self-center">
-                                <h3 class="m-b-0">342</h3>
-                                <h5 class="text-muted m-b-0">New Products</h5>
+                                <h3 class="m-b-0">3</h3>
+                                <h5 class="text-muted m-b-0">New Chamas</h5>
                             </div>
                         </div>
                     </div>
@@ -72,7 +71,7 @@
                             <div class="round align-self-center round-danger"><i class="ti-calendar"></i>
                             </div>
                             <div class="m-l-10 align-self-center">
-                                <h3 class="m-b-0">56%</h3>
+                                <h3 class="m-b-0">16%</h3>
                                 <h5 class="text-muted m-b-0">Today's Profit</h5>
                             </div>
                         </div>
@@ -88,8 +87,8 @@
                             <div class="round align-self-center round-success"><i class="ti-settings"></i>
                             </div>
                             <div class="m-l-10 align-self-center">
-                                <h3 class="m-b-0">56%</h3>
-                                <h5 class="text-muted m-b-0">New Leads</h5>
+                                <h3 class="m-b-0">5</h3>
+                                <h5 class="text-muted m-b-0">New Events</h5>
                             </div>
                         </div>
                     </div>
@@ -109,22 +108,22 @@
             <div class="col-lg-4 col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title ">Leads by Source</h5>
+                        <h5 class="card-title ">Company Analysis</h5>
                         <div id="morris-donut-chart" class="ecomm-donute" style="height: 317px;"></div>
                         <ul class="list-inline m-t-30 text-center">
                             <li class="p-r-20">
                                 <h5 class="text-muted"><i class="fa fa-circle"
-                                        style="color: #fb9678;"></i> Ads</h5>
+                                        style="color: #fb9678;"></i> Amount</h5>
                                 <h4 class="m-b-0">8500</h4>
                             </li>
                             <li class="p-r-20">
                                 <h5 class="text-muted"><i class="fa fa-circle"
-                                        style="color: #01c0c8;"></i> Tredshow</h5>
+                                        style="color: #01c0c8;"></i> Profits</h5>
                                 <h4 class="m-b-0">3630</h4>
                             </li>
                             <li>
                                 <h5 class="text-muted"> <i class="fa fa-circle"
-                                        style="color: #4F5467;"></i> Web</h5>
+                                        style="color: #4F5467;"></i> Expenses</h5>
                                 <h4 class="m-b-0">4870</h4>
                             </li>
                         </ul>
@@ -135,13 +134,13 @@
             <div class="col-lg-8 col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Top Products sales</h5>
+                        <h5 class="card-title">Total revenue Analysis</h5>
                         <ul class="list-inline text-center">
                             <li>
-                                <h5><i class="fa fa-circle m-r-5" style="color: #00bfc7;"></i>iMac</h5>
+                                <h5><i class="fa fa-circle m-r-5" style="color: #00bfc7;"></i>Chamas</h5>
                             </li>
                             <li>
-                                <h5><i class="fa fa-circle m-r-5" style="color: #b4becb;"></i>iPhone</h5>
+                                <h5><i class="fa fa-circle m-r-5" style="color: #b4becb;"></i>Members</h5>
                             </li>
                         </ul>
                         <div id="morris-area-chart2" style="height: 370px;"></div>
@@ -153,79 +152,7 @@
         <!-- Total Leads -->
         <!-- ============================================================== -->
         <!-- .row -->
-        <div class="row">
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><small class="pull-right text-success"><i
-                                    class="fa fa-sort-asc"></i> 18% High then last month</small> Total Leads
-                        </h5>
-                        <div class="stats-row">
-                            <div class="stat-item">
-                                <h6>Overall Growth</h6>
-                                <b>80.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Montly</h6>
-                                <b>15.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Day</h6>
-                                <b>5.50%</b>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="sparkline8" class="minus-mar"></div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><small class="pull-right text-danger"><i
-                                    class="fa fa-sort-desc"></i> 18% High then last month</small>Total Vendor
-                        </h5>
-                        <div class="stats-row">
-                            <div class="stat-item">
-                                <h6>Overall Growth</h6>
-                                <b>80.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Montly</h6>
-                                <b>15.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Day</h6>
-                                <b>5.50%</b>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="sparkline9" class="minus-mar"></div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><small class="pull-right text-success"><i
-                                    class="fa fa-sort-asc"></i> 18% High then last month</small>Invoice</h5>
-                        <div class="stats-row">
-                            <div class="stat-item">
-                                <h6>Overall Growth</h6>
-                                <b>80.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Montly</h6>
-                                <b>15.40%</b>
-                            </div>
-                            <div class="stat-item">
-                                <h6>Day</h6>
-                                <b>5.50%</b>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="sparkline10" class="minus-mar"></div>
-                </div>
-            </div>
-        </div>
+   
         <!-- /.row -->
         <!-- ============================================================== -->
         <!-- End Total Leads -->
@@ -235,11 +162,11 @@
         <!-- ============================================================== -->
         <!-- /row -->
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title m-b-0">New Customers List</h5>
-                        <p class="text-muted">this is the sample data here for crm</p>
+                        <h5 class="card-title m-b-0">New Members List</h5>
+                        <p class="text-muted">Recent members to Wangeshi chama</p>
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>
@@ -271,14 +198,14 @@
                                         <td>Sanghani</td>
                                         <td>Gusikowski</td>
                                         <td>@Govinda</td>
-                                        <td><span class="label label-warning">developer</span> </td>
+                                        <td><span class="label label-warning">Member</span> </td>
                                     </tr>
                                     <tr>
                                         <td>4</td>
                                         <td>Roshan</td>
                                         <td>Rogahn</td>
                                         <td>@Hritik</td>
-                                        <td><span class="label label-success">supporter</span> </td>
+                                        <td><span class="label label-success">Member</span> </td>
                                     </tr>
                                     <tr>
                                         <td>5</td>
@@ -300,7 +227,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6">
+            {{-- <div class="col-sm-6">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title m-b-0">New Product List</h5>
@@ -359,7 +286,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <!-- /.row -->
         <!-- ============================================================== -->
@@ -369,42 +296,7 @@
         <!-- Right sidebar -->
         <!-- ============================================================== -->
         <!-- .right-sidebar -->
-        <div class="right-sidebar">
-            <div class="slimscrollright">
-                <div class="rpanel-title"> Service Panel <span><i
-                            class="ti-close right-side-toggle"></i></span> </div>
-                <div class="r-panel-body">
-                    <ul id="themecolors" class="m-t-20">
-                        <li><b>With Light sidebar</b></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme">1</a>
-                        </li>
-                        <li><a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a>
-                        </li>
-                        <li><a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme">4</a>
-                        </li>
-                        <li><a href="javascript:void(0)" data-skin="skin-purple" class="purple-theme">5</a>
-                        </li>
-                        <li><a href="javascript:void(0)" data-skin="skin-megna" class="megna-theme">6</a>
-                        </li>
-                        <li class="d-block m-t-30"><b>With Dark sidebar</b></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-default-dark"
-                                class="default-dark-theme working">7</a></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-green-dark"
-                                class="green-dark-theme">8</a></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-red-dark" class="red-dark-theme">9</a>
-                        </li>
-                        <li><a href="javascript:void(0)" data-skin="skin-blue-dark"
-                                class="blue-dark-theme">10</a></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-purple-dark"
-                                class="purple-dark-theme">11</a></li>
-                        <li><a href="javascript:void(0)" data-skin="skin-megna-dark"
-                                class="megna-dark-theme ">12</a></li>
-                    </ul>
-                   
-                </div>
-            </div>
-        </div>
+     
         <!-- ============================================================== -->
         <!-- End Right sidebar -->
         <!-- ============================================================== -->
